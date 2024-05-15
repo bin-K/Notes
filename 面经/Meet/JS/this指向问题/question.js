0ecd04b49a420e47f9c0e40f7258255d1abd71ca
kuangzhibin <1520257238@qq.com>
2024-05-15 14:57:05 +0800

    this指向问题

--- 面经/Meet/JS/this指向问题/question.js
+++ 面经/Meet/JS/this指向问题/question.js
@@ -1,4 +1,4 @@
-// 1
+//#region 1
 /*
 var a = 10;
 function foo () {
@@ -6,9 +6,9 @@ function foo () {
 }
 foo();
 */
+//#endregion
 
-// 2
-
+//#region 2
 /*
 "use strict";
 var a = 10;
@@ -21,8 +21,9 @@ console.log(window.foo) // f foo(){...}
 console.log('this2', this) // windiow
 foo();
 */
+//#endregion
 
-// 3
+//#region 3
 
 /* let a = 10
 const b = 20
@@ -34,7 +35,9 @@ function foo () {
 foo();
 console.log(window.a) // undefined */
 
-// 4
+//#endregion
+
+//#region 4
 
 /* var a = 1
 function foo () {
@@ -45,7 +48,9 @@ function foo () {
 
 foo() */
 
-// 5 
+//#endregion
+
+//#region 5
 
 /* var a = 1
 function foo () {
@@ -58,7 +63,9 @@ function foo () {
 
 foo() */
 
-// 6
+//#endregion
+
+//#region 6
 /* function foo () {
   console.log(this.a) // 1
 }
@@ -66,7 +73,9 @@ var obj = { a: 1, foo }
 var a = 2
 obj.foo() */
 
-// 7
+//#endregion
+
+//#region 7
 
 /* function foo () {
   console.log(this.a)
@@ -79,7 +88,9 @@ obj.foo(); // 1
 foo2(); // 2
  */
 
-// 8
+//#endregion
+
+//#region 8
 
 /* function foo () {
   console.log(this.a)
@@ -93,7 +104,9 @@ obj.foo(); // 1
 foo2(); // 2
 obj2.foo2(); // 3 */
 
-// 9
+//#endregion
+
+//#region 9
 /* 
 function foo () {
   console.log(this.a)
@@ -106,7 +119,9 @@ var obj = { a: 1, foo }
 var a = 2
 doFoo(obj.foo) // window 2 */
 
-// 10
+//#endregion
+
+//#region 10
 
 /* function foo () {
   console.log(this.a) // 2
@@ -121,7 +136,9 @@ var obj2 = { a: 3, doFoo }
 
 obj2.doFoo(obj.foo) */
 
-// 11
+//#endregion
+
+//#region 11
 /* "use strict"
 function foo () {
   console.log(this.a) // undefined 没有 a
@@ -136,8 +153,9 @@ var obj2 = { a: 3, doFoo }
 
 obj2.doFoo(obj.foo) */
 
+//#endregion
 
-// 12
+//#region 12
 
 /* function foo () {
   console.log(this.a)
@@ -151,7 +169,9 @@ foo.apply(obj) // 1
 foo.bind(obj)
  */
 
-// 13 
+//#endregion
+
+//#region 13
 
 /* var obj1 = {
   a: 1
@@ -173,7 +193,9 @@ var a = 3
 obj2.foo1() // 2
 obj2.foo2() // window 3 */
 
-// 14
+//#endregion
+
+//#region 14
 
 // var obj1 = {
 //   a: 1
@@ -194,7 +216,9 @@ obj2.foo2() // window 3 */
 // obj2.foo1() // 2
 // obj2.foo2() // obj1 1
 
-// 15
+//#endregion
+
+//#region 15
 
 /* var obj1 = {
   a: 1
@@ -216,7 +240,9 @@ var a = 3
 obj2.foo1() // 2
 obj2.foo2() // window 3 */
 
-// 16
+//#endregion
+
+//#region 16
 /* 
 function foo () {
   console.log(this.a)
@@ -228,7 +254,9 @@ foo() // 2
 foo.call(obj) // 1
 foo().call(obj) // 2,  Cannot read property 'call' of undefined */
 
-// 17
+//#endregion
+
+//#region 17
 
 /* function foo () {
   console.log(this.a)
@@ -243,7 +271,9 @@ foo() // 2
 foo.call(obj) // 1
 foo().call(obj) // 2 1 */
 
-// 18
+//#endregion
+
+//#region 18
 /* 
 function foo () {
   console.log(this.a)
@@ -259,7 +289,9 @@ foo.bind(obj)
 foo().bind(obj) // 2
  */
 
-// 19
+//#endregion
+
+//#region 19
 
 // function foo () {
 //   console.log(this.a)
@@ -272,7 +304,9 @@ foo().bind(obj) // 2
 
 // foo.call(obj)() // 1 2
 
-// 20
+//#endregion
+
+//#region 20
 /* var obj = {
   a: 'obj',
   foo: function () {
@@ -289,7 +323,9 @@ obj.foo()() // 'foo:obj' 'inner:window'
 obj.foo.call(obj2)() // 'foo:obj2' 'inner:window'
 obj.foo().call(obj2) // foo:'obj' 'inner:obj2' */
 
-// 21
+//#endregion
+
+//#region 21
 
 /* var obj = {
   a: 1,
@@ -306,7 +342,9 @@ var obj2 = { a: 3 }
 obj.foo(a).call(obj2, 1) // 6
 obj.foo.call(obj2)(1) // 6 */
 
-// 22
+//#endregion
+
+//#region 22
 
 /* function foo1 () {
   console.log(this.a)
@@ -323,7 +361,9 @@ var foo2 = function () {
 foo2() // 2
 foo2.call(window) // 2 */
 
-// 23
+//#endregion
+
+//#region 23
 
 /* function foo1 (b) {
   console.log(`${this.a} + ${b}`)
@@ -341,7 +381,9 @@ var foo2 = function () {
 var num = foo2(3) // 2 + 3
 console.log(num) // 5 */
 
-// 24
+//#endregion
+
+//#region 24
 
 /* function foo (item) {
   console.log(item, this.a)
@@ -359,7 +401,9 @@ arr.filter(function (i) {
   return i > 2
 }, obj) */
 
-// 25
+//#endregion
+
+//#region 25
 
 /* function Person (name) {
   this.name = name
@@ -368,7 +412,9 @@ var name = 'window'
 var person1 = new Person('LinDaiDai')
 console.log(person1.name) // 'LinDaiDai' */
 
-// 26
+//#endregion
+
+//#region 26
 
 /* function Person (name) {
   this.name = name
@@ -385,7 +431,9 @@ var person1 = new Person('person1')
 person1.foo1() // person1
 person1.foo2()() // undefined */
 
-// 27
+//#endregion
+
+//#region 27
 /* 
 var name = 'window'
 function Person (name) {
@@ -411,7 +459,9 @@ var person1 = new Person('person1')
 person1.foo()() // 'person1' window
 person2.foo()() // 'person2' window */
 
-// 28
+//#endregion
+
+//#region 28
 
 /* var name = 'window'
 function Person (name) {
@@ -429,7 +479,9 @@ var person2 = new Person('person2')
 person1.foo.call(person2)() // 'person2' window
 person1.foo().call(person2) // 'person1' 'person2' */
 
-// 29
+//#endregion
+
+//#region 29
 
 /* var obj = {
   name: 'obj',
@@ -447,7 +499,9 @@ var name = 'window'
 obj.foo1() // window
 obj.foo2()() // obj obj */
 
-// 30
+//#endregion
+
+//#region 30
 /* var name = 'window'
 var obj1 = {
 	name: 'obj1',
@@ -466,8 +520,10 @@ var obj2 = {
 obj1.foo() // obj1
 obj2.foo() // window */
 
-// 31
-var name = 'window'
+//#endregion
+
+//#region 31
+/* var name = 'window'
 var obj1 = {
   name: 'obj1',
   foo: function () {
@@ -507,8 +563,294 @@ var obj4 = {
 
 obj1.foo()() // obj1 window
 obj2.foo()() // obj2 obj2
-obj3.foo()() // obj
-obj4.foo()()
+obj3.foo()() // window window
+obj4.foo()() // window window */
+
+//#endregion
+
+//#region 32
+
+/* var name = 'window'
+function Person (name) {
+  this.name = name
+  this.foo1 = function () {
+    console.log(this.name)
+  }
+  this.foo2 = () => {
+    console.log(this.name)
+  }
+}
+var person2 = {
+  name: 'person2',
+  foo2: () => {
+    console.log(this.name)
+  }
+}
+var person1 = new Person('person1')
+person1.foo1() // person1
+person1.foo2() // person1
+person2.foo2() // window */
+
+//#endregion
+
+//#region 33
+/* var name = 'window'
+function Person (name) {
+  this.name = name
+  this.foo1 = function () {
+    console.log(this.name)
+    return function () {
+      console.log(this.name)
+    }
+  }
+  this.foo2 = function () {
+    console.log(this.name)
+    return () => {
+      console.log(this.name)
+    }
+  }
+  this.foo3 = () => {
+    console.log(this.name)
+    return function () {
+      console.log(this.name)
+    }
+  }
+  this.foo4 = () => {
+    console.log(this.name)
+    return () => {
+      console.log(this.name)
+    }
+  }
+}
+var person1 = new Person('person1')
+person1.foo1()() // person1 window
+person1.foo2()() // person1 person1
+person1.foo3()() // person1 window
+person1.foo4()() // person1 person1 */
+
+//#endregion
+
+//#region 34
+/* var name = 'window'
+var obj1 = {
+  name: 'obj1',
+  foo1: function () {
+    console.log(this.name)
+    return () => {
+      console.log(this.name)
+    }
+  },
+  foo2: () => {
+    console.log(this.name)
+    return function () {
+      console.log(this.name)
+    }
+  }
+}
+var obj2 = {
+  name: 'obj2'
+}
+obj1.foo1.call(obj2)() // obj2 obj2
+obj1.foo1().call(obj2) // obj1 obj1
+obj1.foo2.call(obj2)() // window window
+obj1.foo2().call(obj2) // window obj2 */
+
+//#endregion
+
+//#region 35
+/* var name = 'window'
+var person1 = {
+  name: 'person1',
+  foo1: function () {
+    console.log(this.name)
+  },
+  foo2: () => console.log(this.name),
+  foo3: function () {
+    return function () {
+      console.log(this.name)
+    }
+  },
+  foo4: function () {
+    return () => {
+      console.log(this.name)
+    }
+  }
+}
+var person2 = { name: 'person2' }
+
+person1.foo1() // person1
+person1.foo1.call(person2) // preson2
+
+person1.foo2() // window
+person1.foo2.call(person2) // window
+
+person1.foo3()() // window
+person1.foo3.call(person2)() // window
+person1.foo3().call(person2) // person2
+
+person1.foo4()() // person1
+person1.foo4.call(person2)() // person2
+person1.foo4().call(person2) // person1 */
+//#endregion
+
+//#region 36
+
+/* var name = 'window'
+function Person (name) {
+  this.name = name
+  this.foo1 = function () {
+    console.log(this.name)
+  },
+  this.foo2 = () => console.log(this.name),
+  this.foo3 = function () {
+    return function () {
+      console.log(this.name)
+    }
+  },
+  this.foo4 = function () {
+    return () => {
+      console.log(this.name)
+    }
+  }
+}
+var person1 = new Person('person1')
+var person2 = new Person('person2')
+
+person1.foo1() // person1
+person1.foo1.call(person2) // person2
+
+person1.foo2() // person1
+person1.foo2.call(person2) // person1
+
+person1.foo3()() // winodw
+person1.foo3.call(person2)() // window
+person1.foo3().call(person2) // person2
+
+person1.foo4()() // person1
+person1.foo4.call(person2)() // person2
+person1.foo4().call(person2) // person1 */
+
+//#endregion
+
+//#region 37
+
+/* var name = 'window'
+function Person (name) {
+  this.name = name
+  this.obj = {
+    name: 'obj',
+    foo1: function () {
+      return function () {
+        console.log(this.name)
+      }
+    },
+    foo2: function () {
+      return () => {
+        console.log(this.name)
+      }
+    }
+  }
+}
+var person1 = new Person('person1')
+var person2 = new Person('person2')
+
+person1.obj.foo1()() // window
+person1.obj.foo1.call(person2)() // window
+person1.obj.foo1().call(person2) // person2
+
+person1.obj.foo2()() // obj
+person1.obj.foo2.call(person2)() // person2
+person1.obj.foo2().call(person2) //  obj */
+
+//#endregion
+
+//#region 38
+
+/* function foo() {
+  console.log( this.a ); // 2
+}
+var a = 2;
+(function(){
+  "use strict";
+  foo();
+})(); */
+
+//#endregion
+
+
+//#region 手写new
+function New(fn, ...args) {
+  // 创建一个空的对象并链接到构造函数的原型，使它能访问原型中的属性
+  const instance = Object.create(fn.prototype)
+  // 使用apply改变构造函数中this的指向实现继承，使obj能访问到构造函数中的属性
+  const res = fn.apply(instance, args)
+  // 优先返回构造函数返回的对象
+  return typeof res === 'object' || typeof res === 'function' ? res : instance
+}
+
+function Person (name) {
+  this.name = name
+}
+Person.prototype.eat = function () {
+  console.log('Eatting')
+}
+
+var lindaidai = New(Person, 'LinDaiDai')
+console.log(lindaidai) // Person{ name: 'LinDaiDai' }
+lindaidai.eat() // 'Eatting'
+//#endregion
+
+//#region 手写Call, 手写Apply 都一样
+Function.prototype.Call = function (context, ...args) {
+  if (!context || context === null) {
+    context = window
+  }
+
+  let fn = Symbol()
+
+  context[fn] = this
+
+  const res = context[fn](...args)
+
+  delete context[fn]
+
+  return res
+}
+
+var obj = {
+  name: "objName"
+};
+
+function consoleInfo(sex, weight) {
+  console.log(this.name, sex, weight);
+}
+var name = "globalName";
+consoleInfo.Call(obj, "man", 100); // 'objName' 'man' 100
+consoleInfo.Call(obj, "woman", 120); // 'objName' 'woman' 120
+//#endregion
+
+//#region 手写bind
+Function.prototype.Bind = function (context, ...args) {
+  if (!context || context === null) {
+    context = window
+  }
+
+  let f = Symbol()
+  context[f] = this
+  return function (...args1) {
+    const res = context[f](...args, ...args1)
+    return res
+  }
+}
+
+consoleInfo.Bind(obj, "man", 100)(); // 'objName' 'man' 100
+consoleInfo.Bind(obj, "woman", 120)(); // 'objName' 'woman' 120
+//#endregion
+
+
+
+
+
 
 
 
