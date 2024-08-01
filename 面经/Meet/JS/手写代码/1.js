309c9c1f95f73ea0e9397fa92d517d0ca98fd804
kuangzhibin <1520257238@qq.com>
2024-08-01 17:32:01 +0800

    手写代码1

---
+++ 面经/Meet/JS/手写代码/1.js
@@ -0,0 +1,350 @@
+Function.prototype.Call = function (context, ...args) {
+	if (!context) context = window
+	const f = Symbol()
+	context[f] = this
+	const res = context[f](...args)
+	delete context[f]
+	return res
+}
+
+Function.prototype.Apply = function (context, args) {
+	if (!context) context = window
+	const f = Symbol()
+	context[f] = this
+	const res = context[f](args)
+	delete context[f]
+	return res
+}
+
+Function.prototype.Bind = function (context, ...args) {
+	if (!context) context = window
+	const f = Symbol()
+	context[f] = this
+	return function (...args1) {
+		const res = context[f](...args, ...args1)
+		delete context[f]
+		return res
+	}
+}
+
+function New(fn, ...args) {
+	const instance = Object.create(fn.prototype)
+	const res = fn.apply(instance, args)
+	return typeof res === 'function' || typeof res === 'object' ? res : instance
+}
+
+function debounce(fn, wait) {
+	let timer
+	return function () {
+		const _this = this
+		const args = arguments
+		if (timer) clearTimeout(timer)
+		timer = setTimeout(() => {
+			fn.apply(_this, args)
+		}, wait)
+	}
+}
+
+function throttle(fn, delay) {
+	let curTime = Date.now()
+	return function () {
+		let nowTime = Date.now()
+		if (nowTime - curTime >= delay) {
+			curTime = Date.now()
+			fn.apply(this, arguments)
+		}
+	}
+}
+
+function throttleSetTimeout(fn, delay) {
+	let timer
+	return function () {
+		const _this = this
+		const args = arguments
+		if (!timer) {
+			timer = setTimeout(() => {
+				timer = null
+				fn.apply(_this, args)
+			}, delay)
+		}
+	}
+}
+
+function Foo() {}
+const foo = new Foo()
+
+foo.constructor === Foo
+foo.__proto__ === Foo.prototype
+Foo.prototype.constructor === Foo
+Foo.prototype.__proto__ === Object.prototype
+Object.prototype.constructor === Object
+Object.prototype.__proto__ === null
+
+Foo.constructor === Function
+Foo.__proto__ === Function.prototype
+Object.constructor === Function
+Object.__proto__ === Function.prototype
+Function.prototype.constructor === Function
+Function.prototype.__proto__ === Object.prototype
+
+Function.constructor === Function
+Function.__proto__ === Function.prototype
+
+function Instanceof(context, fn) {
+	const proto = Object.getPrototypeOf(context)
+	if (proto) {
+		if (proto === fn.prototype) {
+			return true
+		} else {
+			return Instanceof(proto, fn)
+		}
+	} else {
+		return false
+	}
+}
+
+function Super() {}
+function Sub() {}
+Sub.prototype = new Super()
+
+function Super() {}
+function Sub() {
+	Super.call(this)
+}
+
+function Super() {}
+function Sub() {
+	Super.call(this)
+}
+Sub.prototype = new Super()
+
+function inheritObject(o) {
+	function F() {}
+	F.prototype = o
+	return new F()
+}
+
+function createObject(o) {
+	let p = inheritObject(o)
+	p.name = 'child'
+	return p
+}
+
+function inheritProtype(SuperClass, SubClass) {
+	let p = inheritObject(SuperClass.prototype)
+	p.prototype = SubClass
+	SubClass.constructor = p
+}
+
+class MyPromise {
+	constructor(executor) {
+		this.state = 'pending'
+		this.data = undefined
+		this.error = undefined
+		this.resovleTask = []
+		this.rejectTask = []
+
+		try {
+			executor(this.resolve.bind(this), this.reject.bind(this))
+		} catch (e) {
+			this.reject(e)
+		}
+	}
+	resolve = (value) => {
+		if (this.state !== 'pending') return
+		this.state = 'fulfilled'
+		this.data = value
+		setTimeout(() => {
+			this.resovleTask.forEach((cb) => cb())
+		})
+	}
+
+	reject = (error) => {
+		if (this.state !== 'pending') return
+		this.error = error
+		setTimeout(() => {
+			this.rejectTask.forEach((cb) => cb())
+		})
+	}
+
+	then = (onResloved, onReject) => {
+		onResloved =
+			typeof onResloved === 'function' ? onResloved : (value) => value
+		onReject =
+			typeof onReject === 'function'
+				? onReject
+				: (error) => {
+						throw error
+				  }
+
+		return new MyPromise((reslove, reject) => {
+			this.resovleTask.push(() => {
+				const res = onResloved(this.data)
+				if (res instanceof MyPromise) {
+					res.then(reslove, reject)
+				} else {
+					reslove(res)
+				}
+			})
+			this.rejectTask.push(() => {
+				const res = onReject(this.error)
+				if (res instanceof MyPromise) {
+					res.then(reslove, reject)
+				} else {
+					reject(res)
+				}
+			})
+		})
+	}
+
+	catch = (onReject) => {
+		this.then(undefined, onReject)
+	}
+
+	static reslove = (value) => {
+		return new MyPromise((reslove, reject) => {
+			if (value instanceof MyPromise) {
+				value.then(reslove, reject)
+			} else {
+				reslove(value)
+			}
+		})
+	}
+
+	static reject = (error) => {
+		return new MyPromise((reslove, reject) => {
+			if (error instanceof MyPromise) {
+				error.then(reslove, reject)
+			} else {
+				reject(error)
+			}
+		})
+	}
+
+	static race = (promises) => {
+		return new MyPromise((reslove, reject) => {
+			for (let i = 0; i < promises.length; i++) {
+				MyPromise.reslove(promises[i]).then(
+					(res) => {
+						reslove(res)
+					},
+					(err) => {
+						reject(err)
+					}
+				)
+			}
+		})
+	}
+
+	static all = (promises) => {
+		let index = 0
+		const result = []
+		return new MyPromise((reslove, reject) => {
+			for (let i = 0; i < promises.length; i++) {
+				MyPromise.reslove(promises[i]).then(
+					(res) => {
+						result[i] = res
+						index++
+						if (index === promises.length) {
+							reslove(result)
+						}
+					},
+					(err) => {
+						reject(err)
+					}
+				)
+			}
+		})
+	}
+
+	static retry = (fn, times, wait) => {
+		return new MyPromise((reslove, reject) => {
+			function func() {
+				MyPromise.prototype(fn()).then(
+					(res) => {
+						reslove(res)
+					},
+					(err) => {
+						if (times > 0) {
+							times--
+							setTimeout(func, wait)
+						} else {
+							reject(err)
+						}
+					}
+				)
+			}
+			func()
+		})
+	}
+}
+
+function asyncToGenerator(generatorFunc) {
+	return function (...args) {
+		const gen = generatorFunc.apply(this, args)
+		return new Promise((reslove, reject) => {
+			function step(key, arg) {
+				let generatorResult
+				try {
+					generatorResult = gen[key](arg)
+				} catch (e) {
+					return reject(e)
+				}
+
+				const { value, done } = generatorResult
+
+				if (done) {
+					reslove(value)
+				} else {
+					Promise.resolve(value).then(
+						(res) => {
+							step('next', res)
+						},
+						(err) => {
+							step('throw', err)
+						}
+					)
+				}
+			}
+			step('next')
+		})
+	}
+}
+
+function deepClone(obj) {
+	if (obj === null) return null
+	if (typeof obj !== 'object') return obj
+	if (obj instanceof Function) return obj
+	if (obj instanceof RegExp) {
+		return new RegExp(obj)
+	}
+	if (obj instanceof Date) {
+		return new Date(obj)
+	}
+
+	const newObj = new obj.constructor()
+	for (let key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			newObj[key] = deepClone(obj[key])
+		}
+	}
+	return newObj
+}
+
+function deepCloneWeakMap(target, hash = new WeakMap()) {
+	const isObject = (obj) => typeof obj === 'object' && obj !== null
+	if (isObject(target)) return target
+	if (hash.get(target)) return hash.get(target)
+	const newObj = Array.isArray(target) ? [] : {}
+	hash.set(target, newObj)
+	for (let key in target) {
+		if (target.hasOwnProperty(key)) {
+			if (isObject(target[key])) {
+				newObj[key] = deepCloneWeakMap(target[key], hash)
+			} else {
+				newObj[key] = target[key]
+			}
+		}
+	}
+	return newObj
+}
